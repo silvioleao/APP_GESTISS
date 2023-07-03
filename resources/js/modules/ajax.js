350fefce2c4eb4e403c3feb4b4e5d62f514e7455
Matheus Souza <sertsoftsalg@gmail.com>
2023-07-03 08:16:09 -0300

    Push da API Correta

--- resources/js/modules/ajax.js
+++ resources/js/modules/ajax.js
@@ -1,6 +1,6 @@
 import Cookies from "js-cookie";
-const API_URL = "http://localhost:8000/api";
-//const API_URL = "https://gestiss.sertsoft.com.br/api";
+//const API_URL = "http://localhost:8000/api";
+const API_URL = "https://gestiss.sertsoft.com.br/api";
 
 $.ajaxSetup({
     headers: {
